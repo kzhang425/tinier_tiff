d168c1f031a0b9039213b8ac4ce4e965d4b8b261
kzhang425 <kzhang425@gmail.com>
2023-04-09 00:09:33 -0400

    Progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,43 @@
+// Link modules to library root, important for file structure
 pub mod tiff_decoder;
+
+// Use certain crates and definitions from standard/external crates
+use std::default::Default;
+
+
+// Include definitions of enums and constants here
+pub enum PlanarConfig {
+    Chunky,
+    Planar,
+}
+impl Default for PlanarConfig {
+    fn default() -> Self { Self::Chunky }
+}
+
+pub enum SampleFormat {
+    UInt,
+    Int,
+    IEEEFP,
+    Void,
+}
+impl Default for SampleFormat {
+    fn default() -> Self { Self::UInt }
+}
+
+pub enum FillOrder {
+    Default,
+    Reverse,
+}
+impl Default for FillOrder {
+    fn default() -> Self { Self::Default }
+}
+
+pub enum PhotometricInterpretation {
+    WhiteIsZero,
+    BlackIsZero,
+    RGB,
+    Palette,
+}
+impl Default for PhotometricInterpretation {
+    fn default() -> Self { Self:: BlackIsZero }
+}
--- src/tiff_decoder.rs
+++ src/tiff_decoder.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 mod tests;
+
+use crate::*;
+use std::fs::File;
 // Endian stuff
 
 #[derive(Debug, PartialEq, Eq)]
@@ -35,6 +38,12 @@ impl EndianDeterminer {
     }
 }
 
+// Replacements for #define, basically constants
+const COMPRESSION_NONE: u16 = 1;
+const COMPRESSION_CCITT: u16 = 2;
+
+const ORIENTATION_STANDARD: u8 = 1;
+
 
 // Holding frame data
 #[derive(Default)]
@@ -49,12 +58,12 @@ pub struct TTReaderFrame {
     strip_count: u32,
     samples_per_pixel: u16,
     bits_per_sample: u32,
-    planar_configuration: u16,
-    sample_format: u16,
+    planar_configuration: PlanarConfig,
+    sample_format: SampleFormat,
     image_length: u32,
     orientation: u8,
     fill_order: u8,
-    photometric_interpretation: u32,
+    photometric_interpretation: PhotometricInterpretation,
     is_tiled: bool,
 
     x_resolution: f32,
@@ -63,10 +72,37 @@ pub struct TTReaderFrame {
 
     description: String,
 }
-
 impl TTReaderFrame {
     /// Makes the struct for an empty frame.
     pub fn new() -> Self {
-        unimplemented!();
+        Self {
+            compression: COMPRESSION_NONE,
+            samples_per_pixel: 1,
+            orientation: ORIENTATION_STANDARD,
+            is_tiled: false,
+
+            x_resolution: 1.0,
+            y_resolution: 1.0,
+            resolution_unit: 1,
+            // Every other thing uses the default: either 0s or default implementations of enums
+            ..Default::default()
+        }
     }
 }
+
+pub struct TTReaderFile {
+    file: File,
+    last_error: String,
+    was_error: bool,
+
+    system_byte_order: u8,
+    file_byte_order: u8,
+
+    // Basically using proper "words" and in accordance with tiff file description
+    firstrecord_offset: u32,
+    nextifd_offset: u32,
+    file_size: u64,
+    
+    current_frame: TTReaderFrame,
+}
+
